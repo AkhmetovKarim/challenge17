32a9dfb9f3943d24939bfc1247f3cefe674e817b
Karim <konanfoi@gmail.com>
2025-03-30 18:02:48 -0400

    Task 2: Create SalesRep class and log client totals

--- cc_17.js
+++ cc_17.js
@@ -21,3 +21,36 @@ customer1.addPurchase(600);
 customer1.addPurchase(700);
 console.log(`Total Spent By ${customer1.name}: $${customer1.getTotalSpent()}`);
 
+
+//Task 2 - Create a SalesRep Class
+class SalesRep {
+    constructor(name) {
+        this.name = name;
+        this.clients = [];
+        console.log(`SalesRep created: ${this.name}`);
+    }
+    addClient(customer) {
+        if (customer instanceof Customer) {
+            this.clients.push(customer);
+            console.log(`${customer.name} been added to ${this.name}`);
+        } else {
+            console.error("customer instance can be added");
+        }
+    }
+
+    getClientTotal(name) {
+        const client = this.clients.find(client => client.name === name);
+        if (client) {
+            const total = client.getTotalSpent();
+            console.log(`${client.name} has spent $${total}`);
+            return total;
+        } else {
+            console.log(`Client ${name} not found`);
+            return 0;
+        }
+    }
+}
+
+const rep1 = new SalesRep("Johnny Cash");
+rep1.addClient(customer1);
+rep1.getClientTotal("John Johnson");
